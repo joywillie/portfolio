1fe58fb458ad4983f29a319ba63ee835aa662d51
Joy William <jw42205769@gmail.com>
2026-06-03 12:17:04 +0300

    Change static assets directory to current folder

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const pool = new Pool({
 });
 
 // Serve static assets out of the public folder
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '.')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
